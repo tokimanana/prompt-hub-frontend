f19ae9e402a5b4c29303fcd6fbc79324b29f54a9
tokimanana <tokimananasarobidy@gmail.com>
2026-01-15 11:00:00 +0300

    style: add global styles and Space Grotesk font

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('prompt-hub-frontend');
-}
+export class App {}
