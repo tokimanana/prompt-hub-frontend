c189fc00e87ddb28c4241c25dcb93c43c5b733f9
tokimanana <tokimananasarobidy@gmail.com>
2026-01-19 11:18:11 +0300

    feat(prompt-card): Theming de PrimeNG (Custom Preset)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,25 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const promptPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    }
+  }
+})
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +30,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: promptPreset
       }
     })
   ]
