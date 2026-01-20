f579144f8fd487e7e60ff3ecf51c2227c13b4d83
tokimanana <tokimananasarobidy@gmail.com>
2026-01-20 11:41:13 +0300

    feat(prompt-form): reactive-form ready

--- src/app/prompts/prompt-form/prompt-form.ts
+++ src/app/prompts/prompt-form/prompt-form.ts
@@ -5,10 +5,12 @@ import { Textarea } from 'primeng/textarea'
 import { SelectModule } from 'primeng/select'
 import { CategoryService } from '../category-service'
 import { toSignal } from '@angular/core/rxjs-interop'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-prompt-form',
-  imports: [Card, InputTextModule, SelectModule, Textarea],
+  imports: [Card, InputTextModule, SelectModule, Textarea, ReactiveFormsModule, Button],
   templateUrl: './prompt-form.html',
   styleUrl: './prompt-form.scss',
 })
@@ -16,4 +18,14 @@ export class PromptForm {
   categoryService = inject(CategoryService)
 
   categories = toSignal(this.categoryService.getCategoris())
+
+  form = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl(''),
+    categoryId: new FormControl(-1),
+  })
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
