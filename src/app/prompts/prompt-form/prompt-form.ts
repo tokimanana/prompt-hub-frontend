fc1a37b36cc70576d5d6a6acf1f578319262f34b
tokimanana <tokimananasarobidy@gmail.com>
2026-01-20 11:34:22 +0300

    feat(prompt-form): implemented categoris option component

--- src/app/prompts/prompt-form/prompt-form.ts
+++ src/app/prompts/prompt-form/prompt-form.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { Card } from 'primeng/card'
+import { InputTextModule } from 'primeng/inputtext'
+import { Textarea } from 'primeng/textarea'
+import { SelectModule } from 'primeng/select'
+import { CategoryService } from '../category-service'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-prompt-form',
-  imports: [],
+  imports: [Card, InputTextModule, SelectModule, Textarea],
   templateUrl: './prompt-form.html',
   styleUrl: './prompt-form.scss',
 })
-export class PromptForm {}
+export class PromptForm {
+  categoryService = inject(CategoryService)
+
+  categories = toSignal(this.categoryService.getCategoris())
+}
