d45455586399cc246d65708e53e9a24d3e5227a8
tokimanana <tokimananasarobidy@gmail.com>
2026-01-21 10:33:54 +0300

    feat(prompt-form): Validators and error handling

--- src/app/prompts/prompt-form/prompt-form.ts
+++ src/app/prompts/prompt-form/prompt-form.ts
@@ -5,7 +5,7 @@ import { Textarea } from 'primeng/textarea'
 import { SelectModule } from 'primeng/select'
 import { CategoryService } from '../category-service'
 import { toSignal } from '@angular/core/rxjs-interop'
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { Button } from 'primeng/button'
 import { PromptService } from '../prompt-service'
 import { Router, RouterLink } from '@angular/router'
@@ -24,12 +24,13 @@ export class PromptForm {
   categories = toSignal(this.categoryService.getCategoris())
 
   form = new FormGroup({
-    title: new FormControl('', { nonNullable: true }),
-    content: new FormControl('', { nonNullable: true }),
-    categoryId: new FormControl(-1, { nonNullable: true }),
+    title: new FormControl('', { validators: [Validators.required, Validators.max(30)], nonNullable: true }),
+    content: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    categoryId: new FormControl(-1, { validators: [Validators.required, Validators.min(6)],  nonNullable: true }),
   })
 
   submit() {
+    if(this.form.invalid) return
     console.log(this.form.value)
     const prompt = this.form.getRawValue()
     this.promptService.createPrompt(prompt).subscribe(() => this.router.navigate(['/']))
