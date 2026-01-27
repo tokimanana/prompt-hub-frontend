a104113c58b2d39174efbb93405eddf309209164
tokimanana <tokimananasarobidy@gmail.com>
2026-01-27 10:20:14 +0300

    feat(auth): canEdit (computed of prompt&&currrentUser)

--- src/app/prompts/prompt-card/prompt-card.ts
+++ src/app/prompts/prompt-card/prompt-card.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core'
+import { Component, input, inject, computed } from '@angular/core';
 import { Prompt } from '../prompt.model'
 import { ButtonModule } from 'primeng/button'
 import { Textarea } from 'primeng/textarea'
 import { TagModule } from 'primeng/tag'
 import { CardModule } from 'primeng/card'
 import { RouterLink } from "@angular/router";
+import { AuthService } from '../../auth/auth-service';
 
 @Component({
   selector: 'app-prompt-card',
@@ -13,7 +14,13 @@ import { RouterLink } from "@angular/router";
   styleUrl: './prompt-card.scss',
 })
 export class PromptCard {
+  authService = inject(AuthService)
   prompt = input.required<Prompt>()
+
+  canEdit = computed(() => {
+    const currentUser = this.authService.currentUser();
+    return currentUser && this.prompt().author.id === currentUser.id
+  })
 
   copyToClipboard() {
     void navigator.clipboard.writeText(this.prompt().content)
