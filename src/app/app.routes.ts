b92061f01b9d9a23996637fe74f4987b6c379c3f
tokimanana <tokimananasarobidy@gmail.com>
2026-01-29 11:02:37 +0300

    feat(auth): protect routes with Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router'
 import { PromptList } from './prompts/prompt-list/prompt-list'
 import { PromptForm } from './prompts/prompt-form/prompt-form'
 import { AuthForm } from './auth/auth-form/auth-form'
+import { authGuard } from './auth/auth-guard'
 
 export const routes: Routes = [
   {
@@ -20,9 +21,11 @@ export const routes: Routes = [
   {
     path: 'prompts/create',
     component: PromptForm,
+    canActivate: [authGuard]
   },
   {
     path: 'prompts/:promptId/edit',
     component: PromptForm,
+    canActivate: [authGuard]
   },
 ]
